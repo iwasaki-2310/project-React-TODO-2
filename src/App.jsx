a9e7c97388cbc62da129ddd128415cdcaabbe737
iwasaki2310 <118096073+iwasaki-2310@users.noreply.github.com>
2023-10-01 05:01:59 +0000

    完了、削除、戻す機能実装完了

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,47 @@ import "./styles.css";
 
 export const App = () => {
   const [todoText, setTodoText] = useState('');
-  const [incompleteTodos, setincompleteTodos] = useState([
-    'あああ', 'いいい'
-  ]);
+  const [inCompleteTodos, setIncompleteTodos] = useState([]);
 
-  const [completeTodos, setcompleteTodos] = useState(['ううう']);
+  const [completeTodos, setCompleteTodos] = useState([]);
 
+  // 入力内容取得処理
   const onChangeTodoText = (event) => setTodoText(event.target.value);
 
+  // 追加処理
   const onClickAdd = () => {
     if(todoText === "") return;
-    const newTodos = [...incompleteTodos, todoText];
-    setincompleteTodos(newTodos);
+    const newTodos = [...inCompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
     setTodoText("");
   }
 
+  // 削除処理
   const onClickDelete = (index) => {
-    const newTodos = [...incompleteTodos];
-    newTodos.splice(index, 1);
-    setincompleteTodos(newTodos);
+    const newTodos = [...inCompleteTodos];
+    newTodos.splice(index, 1);//[1]取り除きたい要素のindex番号, [2]そのindex番号から何個取り除きたいか（指定のない場合は第一引数以降の要素が全て取り除かれる）
+    setIncompleteTodos(newTodos);
   }
 
+  // 完了処理
   const onClickComplete = (index) => {
-    const newIncopleteTodos = [...incompleteTodos];
+    const newIncopleteTodos = [...inCompleteTodos];
     newIncopleteTodos.splice(index, 1);
-    setincompleteTodos(newTodos);
 
-    const newTodos = [...completeTodos];
+    const newCompleteTodos = [...completeTodos, inCompleteTodos[index]];
+    setIncompleteTodos(newIncopleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  }
 
+  // 戻す処理
+  const onClickRemove = (index) => {
+    const newCompleteTodos = [ ...completeTodos];//現在の完了リストを取得
+    newCompleteTodos.splice(index, 1);//完了リストから押下したTODOの削除
+
+    const newInCompleteTodos = [...inCompleteTodos, completeTodos[index]];//未完了リストの配列に押下したindex番号のTODOを追加
+
+    setCompleteTodos(newCompleteTodos);//完了リストを更新
+    setIncompleteTodos(newInCompleteTodos);//未完了リストを更新
   }
 
   return (
@@ -43,7 +56,7 @@ export const App = () => {
       <div className="area incomplete-area">
         <p className="title-area">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo, index) =>{
+          {inCompleteTodos.map((todo, index) =>{
             return (
               <li key={todo} className="list-row">
                 <p>{todo}</p>
@@ -58,11 +71,11 @@ export const App = () => {
       <div className="area complete-area">
         <p className="title-area">完了のTODO</p>
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return(
               <li key={todo} className="list-row">
                 <p>{todo}</p>
-                <button>戻す</button>
+                <button onClick={() => onClickRemove(index)}>戻す</button>
               </li>
             );
           })}
