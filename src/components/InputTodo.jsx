b2dfe74f8549661b9c7ca16938747b40a28ee199
iwasaki2310 <118096073+iwasaki-2310@users.noreply.github.com>
2023-10-08 05:29:06 +0000

    入力エリアにCSS-in-JSを実装

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+const style = {
+  margin: '15px 0 0',
+  padding: '15px',
+  borderRadius: '10px',
+  background: 'rgb(188, 222, 240)'
+}
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="area input-area">
+    <div style={style} className="area input-area">
       <input
         type="text"
         placeholder="TODOを入力"
